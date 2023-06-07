cfec48d04807c27d6f87718fe2e1346d9c4ffe75
Dev01 <guilhermebrasil.acs@outlook.com>
2023-06-07 11:07:00 -0300

    Gateway

---
+++ src/test/java/com/projectapi/ACSchapter12/apigateway/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectapi.ACSchapter12.apigateway.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
